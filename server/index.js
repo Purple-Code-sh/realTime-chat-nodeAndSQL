3f1b87eb298043dfbd251bed3988809847b952f6
Purple-Code-sh <purple.code.sh@gmail.com>
2024-02-20 18:09:47 -0600

    creating the client

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 import express from 'express'
+import loger from 'morgan'
 
-const port = process.env.PORT ?? 3000
+const port = process.env.PORT ?? 1999
 
 const app = express()
 
+app.use(loger('dev'))
+
 app.get('/', (req,res)=>{
-    res.send('<h1> Welcome to the chat</h1>')
+    res.sendFile(process.cwd() + '/client/index.html')
 })
 
 app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 })
