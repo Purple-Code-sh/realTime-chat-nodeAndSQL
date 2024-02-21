be25fef26faa900998dac3fc9be759c52949aa8f
Purple-Code-sh <purple.code.sh@gmail.com>
2024-02-20 18:27:49 -0600

    add standard and slint config

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 import express from 'express'
 import loger from 'morgan'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
 const port = process.env.PORT ?? 1999
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
 
 app.use(loger('dev'))
 
-app.get('/', (req,res)=>{
-    res.sendFile(process.cwd() + '/client/index.html')
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, ()=>{
-    console.log(`Server running on http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
 })
