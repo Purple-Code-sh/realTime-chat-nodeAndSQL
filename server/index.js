e468aad3b9ed51a333357c45be8bc3139d8c5cce
Purple-Code-sh <purple.code.sh@gmail.com>
2024-02-21 18:34:50 -0600

    config database connection, and connect server to its

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ import express from 'express'
 import loger from 'morgan'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+import { createClient } from '@libsql/client'
+
+import dotenv from 'dotenv'
+dotenv.config()
 
 const port = process.env.PORT ?? 1999
 
@@ -11,6 +15,18 @@ const io = new Server(server, {
   connectionStateRecovery: {}
 })
 
+const db = createClient({
+  url: 'libsql://working-huntress-purple-code-sh.turso.io',
+  authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT
+  )
+`)
+
 io.on('connection', (socket) => {
   console.log('An user has connected')
 
@@ -18,8 +34,18 @@ io.on('connection', (socket) => {
     console.log('An user has disconnected')
   })
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg)
+  socket.on('chat message', async (msg) => {
+    let result
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content) VALUES (:msg)',
+        args: { msg }
+      })
+    } catch (error) {
+      console.error(error)
+      return
+    }
+    io.emit('chat message', msg, result.lastInsertRowid.toString())
   })
 })
 
